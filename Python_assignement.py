91c9746681f32e849c2ff7e023136208124fad4e
cindymenp <52049218+cindymenp@users.noreply.github.com>
2023-08-16 08:46:20 +0200

    Python_assignement.py

--- Python_assignement.py
+++ Python_assignement.py
@@ -11,36 +11,35 @@ Create and call a python function that :
 
 import random
 
+def integers():
 
-while True:
+    while True:
 
-    #stores a random integer A between 1 and 9
-    Integer_A = random.randint(0, 9)
+        #stores a random integer A between 1 and 9
+        Integer_A = random.randint(0, 9)
 
-    #stores a random integer B between 1 and 9
-    Integer_B = random.randint(0, 9)
- 
-    #multiplies A and B together as C
-    Integer_C = Integer_A * Integer_B
-
-
-    #Prints A and C for every result until C = 4
-
-    for i in range(Integer_C):
-        print(Integer_A, Integer_C)
-        break
-
-    #If C = 4 , print ‘Success!’ and the results for A and B
-
-    if Integer_C == 4:
-        print("‘Success!’ and the results for" ,Integer_A,"and" ,Integer_B)
-        break
+        #stores a random integer B between 1 and 9
+        Integer_B = random.randint(0, 9)
     
+        #multiplies A and B together as C
+        Integer_C = Integer_A * Integer_B
 
-    
+
+        #Prints A and C for every result until C = 4
+
+        for i in range(Integer_C):
+            print("A = ",Integer_A, "C = ", Integer_C)
+            break
+
+        #If C = 4 , print ‘Success!’ and the results for A and B
+
+        if Integer_C == 4:
+            print("‘Success!’ A = " ,Integer_A,"and" ,"B = ",Integer_B)
+            break
         
-     
-   
+    return 
+
+integers() 
         
  
         
